87a4cabbb80b0d725e908067fceb10294cc1151b
MICAH ADDAH <addahmicah@gmail.com>
2022-06-05 16:25:15 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first_num;
+	int second_num;
+
+	for (second_num = '0'; second_num <= '9'; second_num++)
+	{
+		for (first_num = (second_num + 1); first_num <= '9'; first_num++)
+		{
+			putchar(second_num);
+			putchar(first_num);
+
+			if (second_num != '8' || first_num != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
