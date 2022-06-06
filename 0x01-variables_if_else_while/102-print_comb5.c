54def5379bdeeaef6bfad58126a606b7b6f3042c
MICAH ADDAH <addahmicah@gmail.com>
2022-06-06 14:51:17 -0700

    Last combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			for (num3 = num1; num3 <= '9'; num3++)
 			{
-				for (num4 = (num2 + 1); num4 <= '9'; num4++)
+				for (num4 = num2 + 1; num4 <= '9'; num4++)
 				{
 					putchar(num1);
 					putchar(num2);
