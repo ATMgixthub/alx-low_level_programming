7de5eb771d196f376dbcb354273f9ce0f6915647
MICAH ADDAH <addahmicah@gmail.com>
2022-06-08 14:45:47 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long a = 1;
+	long b = 2;
+
+	for (i = 1; i <= (50 / 2); i++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+	if (50 % 2 == 1)
+		printf("li", a);
+
+	printf('\n');
+
+	return (0);
+}
