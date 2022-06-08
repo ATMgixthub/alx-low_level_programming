0f21a1dd7a5bb362cd0ab0f34394869dd4784a95
MICAH ADDAH <addahmicah@gmail.com>
2022-06-08 08:10:19 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - print multiplication table
+ */
+
+void times_table(void)
+{
+	int row;
+	int column;
+	int multiple;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			multiple = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + multiple);
+			}
+			if (multiple <= 9)
+			{
+				_putchar(',');
+				_puthar(' ');
+				_putchar(' ');
+				_putchar('0' + multiple);
+			}
+			if (multiple > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (multiple / 10));
+				_putchar('0' + (multiple % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
