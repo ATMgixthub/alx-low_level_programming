c8b3a34f630b5e9cdc31f40edf3396d6ad00cf7d
MICAH ADDAH <addahmicah@gmail.com>
2022-06-07 17:17:38 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "functions.h"
+
+/**
+ * main - print _putchar, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar();
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_FILE
+#define FUNCTIONS_FILE
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+#endif
