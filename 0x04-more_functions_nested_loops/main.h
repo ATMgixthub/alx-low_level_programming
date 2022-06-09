fba59f3617cdbfaed457033734d429308773543a
MICAH ADDAH <addahmicah@gmail.com>
2022-06-09 16:40:34 -0700

    Check for a digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,4 +16,23 @@ int _putchar(char c);
 
 int _isupper(int c);
 
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c);
+
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
+int mul(int a, int b);
+
+
+
 #endif
