3aca870f665c0aa4b3a456955a948c0b2821c05e
MICAH ADDAH <addahmicah@gmail.com>
2022-06-09 19:59:42 -0700

    Print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + 48);
+}
