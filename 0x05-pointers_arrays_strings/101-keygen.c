2d17325fbce6a70b8d7336a57813dcd8014974d1
MICAH ADDAH <addahmicah@gmail.com>
2022-06-14 18:43:10 -0700

    Change random password generator code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,88 +8,47 @@
  *
  * Return: Always 0.
  */
-
-/**
- * randomPasswordGenerator - Generates random password
- * @N: Lenght of password
- * Return: Always 0.
- */
-
-void randomPasswordGenerator(int N);
-
-/* Driver code */
 int main(void)
 {
-	/* Length of password to be generated */
-	int N = 10;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	/* Function call */
+	srand(time(0));
 
-	return (0);
-}
-
-/**
- * randomPasswordGenerator - Generates random password
- * @N: Lenght of password
- * Return: Always 0.
- */
-
-void randomPasswordGenerator(int N)
-{
-	/* Initialize counter */
-	int i = 0;
-
-	int randomizer = 0;
-
-	/** Seed the random-number generator with current
-	 *  time so that the number will be different always
-	 */
-	srand((unsigned int)(time(NULL)));
-
-	/* Array of numbers */
-	char numbers[] = "0123456789";
-
-	/* Array of small alphabets */
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-	/* Array of capital letters */
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	/* Array of all the special symbols */
-	char symbols[] = "!@#$^&*?";
-
-	/* Stores the random password */
-	char password[N];
-
-	/* To select the randomizer inside the loop */
-	randomizer = rand() % 4;
-
-	/* Iterate over the range [0, N] */
-	for (i = 0; i < N; i++)
+	while (sum < 2772)
 	{
-		if (randomizer == 1)
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
 		{
-			password[i] = numbers[rand() % 10];
-			randommizer = rand() % 4;
-			printf("%c", password[i]);
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
 		}
-		else if (randomizer == 2)
+		for (index = 0; password[index]; index++)
 		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
 		}
 	}
+
+	printf("%s", password);
+
+	return (0);
 }
