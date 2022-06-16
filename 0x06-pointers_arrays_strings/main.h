36cf1277fb66ae9b4270d56f4da16730cd5dde41
MICAH ADDAH <addahmicah@gmail.com>
2022-06-16 07:45:41 -0700

    Function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -86,4 +86,11 @@ char *leet(char *);
 
 char *rot13(char *);
 
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n);
+
 #endif
