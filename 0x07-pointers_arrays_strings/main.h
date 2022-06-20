041a40e7a82c0c16b8795316fcc69188b002e22a
MICAH ADDAH <addahmicah@gmail.com>
2022-06-20 06:29:40 -0700

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -24,6 +24,7 @@ char *_memset(char *s, char b, unsigned int n);
  * @src: source
  * @n: number of bytes
  * Return: pointer to dest
+ *
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
